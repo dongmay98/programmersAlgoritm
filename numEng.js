6c5b6b86023525134ad6cfa16001ecaa75d7ba8f
dongmay <sdh4202@naver.com>
2023-08-09 18:48:41 +0900

    20230809_algoritm_sdh

---
+++ numEng.js
@@ -0,0 +1,19 @@
+//숫자 문자열과 영단어
+function solution(s) {
+  const word = {
+      zero: 0,
+      nine: 9,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      one: 1
+  };
+  for(var key in word){
+      s = s.replaceAll(key,word[key]);
+  }
+  return parseInt(s);
+}
--- syzerPwd.js
+++ syzerPwd.js
@@ -1,28 +1,30 @@
 //서동현
 //시저암호
-function solution(s, n) {  
+function solution(s, n) {
   let arrS = s.split("");
   let arrCharcode = arrS.map((arr) => arr.charCodeAt(0));
   let arr = [];
 
   for (let i = 0; i < arrCharcode.length; i++) {
-      if (65 <= arrCharcode[i] && arrCharcode[i] <= 90) { // 대문자
-          let shiftedCharCode = arrCharcode[i] + n;
-          if (shiftedCharCode > 90) {
-              shiftedCharCode = shiftedCharCode - 26;
-          }
-          arr.push(shiftedCharCode);
-      } else if (97 <= arrCharcode[i] && arrCharcode[i] <= 122) { // 소문자
-          let shiftedCharCode = arrCharcode[i] + n;
-          if (shiftedCharCode > 122) {
-              shiftedCharCode = shiftedCharCode - 26;
-          }
-          arr.push(shiftedCharCode);
-      } else {
-          arr.push(arrCharcode[i]); // 기타 문자는 그대로 추가
+    if (65 <= arrCharcode[i] && arrCharcode[i] <= 90) {
+      // 대문자
+      let shiftedCharCode = arrCharcode[i] + n;
+      if (shiftedCharCode > 90) {
+        shiftedCharCode = shiftedCharCode - 26;
       }
+      arr.push(shiftedCharCode);
+    } else if (97 <= arrCharcode[i] && arrCharcode[i] <= 122) {
+      // 소문자
+      let shiftedCharCode = arrCharcode[i] + n;
+      if (shiftedCharCode > 122) {
+        shiftedCharCode = shiftedCharCode - 26;
+      }
+      arr.push(shiftedCharCode);
+    } else {
+      arr.push(arrCharcode[i]); // 기타 문자는 그대로 추가
+    }
   }
 
-  let answer= arr.map(code => String.fromCharCode(code)).join("");
+  let answer = arr.map((code) => String.fromCharCode(code)).join("");
   return answer;
 }
